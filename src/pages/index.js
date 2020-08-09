8c0048aa8f7f6e21934adb5e63021def985ce599
youngheart12 <manishkumar199222@gmail.com>
2020-08-09 20:28:13 +0530

    adding initial config files

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Button from '@material-ui/core/Button'
+export default function Home() {
+  return <div>Its my first step towards this
+    <Button color="primary" variant="contained">Hello world</Button>
+    <Button color="secondary">Work load</Button>
+  </div>
+}
